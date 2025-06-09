b7cfdfcc4d87406b3c11b4e332dee4bace2b92cd
Hrithik <hrithikkumar88044@gmail.com>
2025-06-10 02:42:51 +0530

    Fix: use environment variables for Render deployment

--- index.js
+++ index.js
@@ -466,7 +466,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:4000/auth/google/group',
+      callbackURL: 'https://cabclique.onrender.com/auth/google/group',
       userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
     },
     async (accessToken, refreshToken, profile, cb) => {
